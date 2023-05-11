1f41c96abb7f972904d5bd395a6cdf90184e20d4
Arthur Simon <rthrsmn@gmail.com>
2023-05-11 18:12:27 +0200

    db set with user and password => go link nest with db

--- srcs/db/init.sh
+++ srcs/db/init.sh
@@ -1,4 +1,16 @@
 service postgresql start
-sudo -i -u postgres
-createdb test_trans
-psql test_trans
+cat << EOF | adduser $POSTGRES_USER
+$POSTGRES_PASSWORD
+$POSTGRES_PASSWORD
+$POSTGRES_USER
+
+
+
+
+EOF
+
+sudo -u postgres createuser $POSTGRES_USER
+sudo -u postgres createdb $POSTGRES_DB_NAME
+echo "alter user $POSTGRES_USER with encrypted password '$POSTGRES_PASSWORD'; grant all privileges on database $POSTGRES_DB_NAME to $POSTGRES_USER;" | sudo -u postgres psql
+service postgresql stop
+sudo -u postgres /usr/lib/postgresql/11/bin/postgres -D /etc/postgresql/11/main/
