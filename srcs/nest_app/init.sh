2198ec43997970006d166fe5e0ca45f0f1a37aff
Arthur Simon <rthrsmn@gmail.com>
2023-05-13 01:19:11 +0200

    postgres link with nestapp may need to change pg_hba.conf for make nest_app word work

--- srcs/nest_app/init.sh
+++ srcs/nest_app/init.sh
@@ -3,18 +3,17 @@
 # zsh
 
 #install nest
-npm install -g @nestjs/cli
+# npm install -g @nestjs/cli
 
 #update node to the latest
-npm install -g n 
-n latest
+# npm install -g n 
+# n latest
+# hash -r
 
-
-#install typeORM
+# install typeORM
+# cd ft_transcendence/
 # npm install @nestjs/config
 # npm install @nestjs/typeorm typeorm
 # npm install pg
 
-#reload 
-
-nest new $PROJECT_NAME --package-manager npm
+tail -f /dev/null
