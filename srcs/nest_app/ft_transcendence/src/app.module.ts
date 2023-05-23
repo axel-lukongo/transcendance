f5c0c59218d1f077a9fa7d5614424f7a72d490c1
Ilyes DOUIDI <idouidi@e1r3p1.clusters.42paris.fr>
2023-05-23 15:44:23 +0200

    module user in progress

--- srcs/nest_app/ft_transcendence/src/app.module.ts
+++ srcs/nest_app/ft_transcendence/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MessagesModule } from './messages/messages.module';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [UserModule, MessagesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
