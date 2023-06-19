393dbb020c2879d8114e42206de69bb044a17d28
axel <lukongo.axel03@gmail.com>
2023-06-20 01:54:26 +0200

    i can send message instantly

--- srcs/back-end/ft_transcendence/src/app.module.ts
+++ srcs/back-end/ft_transcendence/src/app.module.ts
@@ -5,18 +5,13 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 import { ChanelModule } from './chanel/chanel.module';
 import { MessagesModule } from './messages/messages.module';
-// import { WebSocketModule } from 'graphql-ws/lib/nestjs';
 
 @Module({
 	imports: [
-		// WebSocketModule,
 		GraphQLModule.forRoot<ApolloDriverConfig>({
 			autoSchemaFile: join(process.cwd(), 'src/schemas.gql'),
 			driver: ApolloDriver,
 			playground: true,
-			// subscriptions: {
-			// 	'graphql-ws': true
-			// },
 			installSubscriptionHandlers: true, 
 		}),
 		UsersModule,
--- srcs/front-end/ft_transcendence/src/components/message/message.jsx
+++ srcs/front-end/ft_transcendence/src/components/message/message.jsx
@@ -1,43 +1,92 @@
 
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+// import React from 'react';
+// import { useQuery, gql } from '@apollo/client';
 
-const GET_CHANNEL_CONTENT = gql`
-  query GetChannelContent {
-    Channel_findOne(id: 1) {
-      chanel_name
+// const GET_CHANNEL_CONTENT = gql`
+//   query GetChannelContent {
+//     Channel_findOne(id: 1) {
+//       chanel_name
+//       id
+//       messages {
+//         sender_id
+//         content
+//       }
+//     }
+//   }
+// `;
+
+// const Chat = ({ show }) => {
+// 	const { loading, error, data } = useQuery(GET_CHANNEL_CONTENT);
+
+//   if (loading) {
+//     return <p>Loading...</p>;
+//   }
+
+//   if (error) {
+//     return <p>Error: {error.message}</p>;
+//   }
+
+//   const channel = data.Channel_findOne;
+
+//   return (
+//     <div className={`Chat ${show ? 'show' : ''}`}>
+//       <h1>{channel.chanel_name}</h1>
+//       <ul>
+//         {channel.messages.map((message) => (
+//           <div> user {message.sender_id}: {message.content}</div>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Chat;
+
+import React, { useEffect, useState } from 'react';
+import { gql } from 'graphql-tag';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+
+const wsClient = new SubscriptionClient('ws://localhost:4000/graphql', {});
+
+const NEW_MESSAGE_SUBSCRIPTION = gql`
+  subscription {
+    addmessage {
       id
-      messages {
-        sender_id
-        content
-      }
+      content
+	  sender_id
     }
   }
 `;
 
 const Chat = ({ show }) => {
-	const { loading, error, data } = useQuery(GET_CHANNEL_CONTENT);
+	const [messages, setMessages] = useState([]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  useEffect(() => {
+    const subscription = wsClient.request({ query: NEW_MESSAGE_SUBSCRIPTION }).subscribe({
+      next: (response) => {
+        const newMessage = response.data.addmessage;
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+      },
+      error: (error) => {
+        console.error('WebSocket error:', error);
+      },
+    });
 
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-
-  const channel = data.Channel_findOne;
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
 
   return (
     <div className={`Chat ${show ? 'show' : ''}`}>
-      <h1>{channel.chanel_name}</h1>
+      <h1>Messages en temps réel</h1>
       <ul>
-        {channel.messages.map((message) => (
-          <div> user {message.sender_id}: {message.content}</div>
+        {messages.map((message) => (
+          <div key={message.id}> {message.sender_id} : {message.content}</div>
         ))}
       </ul>
     </div>
   );
-};
+}
 
 export default Chat;
