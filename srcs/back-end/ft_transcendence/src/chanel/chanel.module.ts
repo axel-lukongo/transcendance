37615114c153216ba7a1ec3f0d855501eb434847
axel <lukongo.axel03@gmail.com>
2023-06-25 18:22:08 +0200

    i add the message part in the main

--- srcs/back-end/ft_transcendence/src/chanel/chanel.module.ts
+++ srcs/back-end/ft_transcendence/src/chanel/chanel.module.ts
@@ -3,8 +3,11 @@ import { ChanelService } from './chanel.service';
 import { ChanelResolver } from './chanel.resolver';
 import { PrismaService } from 'prisma/prisma.service';
 import { UsersService } from 'src/users/users.service';
-
+import { MessagesModule } from 'src/messages/messages.module';
+import { MessagesService } from 'src/messages/messages.service';
+import { MessagesResolver } from '../messages/messages.resolver';
 @Module({
-    providers: [ChanelResolver, ChanelService, PrismaService, UsersService]
+	imports: [MessagesModule],
+    providers: [ChanelResolver, ChanelService, PrismaService, UsersService, MessagesService, MessagesResolver]
 })
 export class ChanelModule {}
